78d9c41ee2383251ef4fb7a43168cbf0ef8f3665
ssszcmawo <belan2470@gmail.com>
2025-10-28 23:45:11 +0100

    modified enum,modified functions prototypes

--- slogger.h
+++ slogger.h
@@ -2,50 +2,47 @@
 #define SLOGGER_H
 
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <assert.h>
 #include <pthread.h>
-#include <sys/syscall.h>
 
-enum{
+#define MAX_FILE_NAME 64
+#define DEFAULT_MAX_FILE_SIZE 1048576L
 
-  CONSOLELOGGER = 1 << 0,
-
-  FILELOGGER = 1 << 1,
-
-  maxFileNameLength = 64;
-
-  DefaultMaxFileSize = 1048576L
-
+enum {
+    CONSOLELOGGER = 1 << 0,
+    FILELOGGER    = 1 << 1
 };
 
 typedef enum LogLevel {
-  INFO,
-  DEBUG,
-  WARNING,
-  ERROR
+    INFO,
+    DEBUG,
+    WARNING,
+    ERROR
 } log_level_t;
 
-typedef struct FileLog{
-  FILE* output;
-  char fileName[MAX_FILE_NAME];
-  long maxFileSize;
-  long currentFileSize;
-} file_log_t;
+typedef struct FileLog {
+    FILE* output;
+    char fileName[MAX_FILE_NAME];
+    long maxFileSize;
+    long currentFileSize;
+    log_level_t level;
+} FileLog;
 
-typedef struct ConsoleLog{
-  FILE* output;
-} cons_log_t;
+typedef struct ConsoleLog {
+    FILE* output;
+    log_level_t level;
+} ConsoleLog;
 
+void set_log_level(log_level_t level);
+log_level_t get_log_level(void);
 
 ConsoleLog* init_consoleLog(FILE* output);
 void free_console_log(ConsoleLog* cl);
-void log_message(log_level_t level,const char* text);
-void clear_log_file(const char* filename);
-void set_log_level(log_level_t level);
-void get_log_level();
-void init_logging(const char* filename);
-void close_logging();
 
-#endif // !SLOGGER_H
+FileLog* init_fileLog(const char* filename, long maxFileSize);
+void free_file_log(FileLog* fl);
+
+void log_message(log_level_t level, const char* message);
+void clear_log_file(const char* filename);
+void close_logging(void);
+
+#endif
