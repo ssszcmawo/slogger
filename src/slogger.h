8ae791054c2d34a4c5cca020b6624a4a1afbffad
ssszcmawo <belan2470@gmail.com>
2025-12-05 16:35:03 +0100

    replaced typedefs and constants to .c file,changed return types of init functions,deleted a few functions,added added macros for logging

--- src/slogger.h
+++ src/slogger.h
@@ -7,17 +7,6 @@
 #include <stdbool.h>
 #include <limits.h>
 
-
-#define CONSOLELOGGER 1
-#define FILELOGGER 2
-
-#define MAX_FILE_NAME 64
-#define DEFAULT_MAX_FILE_SIZE 1048576L
-#define MAX_BUF_SIZE 4096
-#define DEFAULT_MAX_BACKUP_FILES 10
-#define LOG_FILENAME_MAX 4096
-#define MAX_LINE_LEN 256
-
 #define COLOR_RESET   "\033[0m"
 #define COLOR_RED     "\033[31m"
 #define COLOR_GREEN   "\033[32m"
@@ -31,35 +20,25 @@ typedef enum LogLevel {
     ERROR
 } log_level_t;
 
-typedef struct FileLog {
-    FILE* output;
-    char fileName[LOG_FILENAME_MAX];
-    long maxFileSize;
-    long currentFileSize;
-    log_level_t level;
-    int maxBackUpFiles;
-    int archiveOldLogs;
-    char archiveDir[LOG_FILENAME_MAX];
-} FileLog;
-
-typedef struct ConsoleLog {
-    FILE* output;
-    log_level_t level;
-} ConsoleLog;
-
 void set_log_level(log_level_t level);
 log_level_t get_log_level(void);
 
-ConsoleLog* init_consoleLog(FILE* output);
-void free_console_log(ConsoleLog* cl);
+int init_consoleLog(FILE* output);
 
-FileLog* init_fileLog(const char* filename, long maxFileSize,bool archiveOrNot);
+int init_fileLog(const char* filename, long maxFileSize,bool archiveOrNot);
 int rotateFiles();
-void free_file_log(FileLog* fl);
-
 
 void log_message(log_level_t level, const char* message);
+void log_messagef(log_level_t level, const char* file, int line, const char* fmt, ...);
 void clear_log_file(const char* filename);
 void close_logging(void);
+
+
+#define LOG_TRACE(fmt, ...) log_messagef(TRACE, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...) log_messagef(DEBUG, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...)  log_messagef(INFO,  __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...)  log_messagef(WARNING, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) log_messagef(ERROR, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+
 
 #endif
