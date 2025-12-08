043ac8fd2c774c0a663ab2c7afa4cc2fe499a48e
ssszcmawo <belan2470@gmail.com>
2025-12-08 18:09:03 +0100

    updated file tests

--- tests/test_file_logger.c
+++ tests/test_file_logger.c
@@ -12,7 +12,7 @@
 
 void cleanup() {
     remove(LOG_FILE);
-    for(int i = 1; i <= DEFAULT_MAX_BACKUP_FILES; i++){
+    for(int i = 1; i <= DEFAULT_MAX_BACKUP_FILES * 10; i++){
         char fname[128];
         snprintf(fname, sizeof(fname), "%s.%d", LOG_FILE, i);
         remove(fname);
@@ -21,17 +21,13 @@ void cleanup() {
     snprintf(cmd, sizeof(cmd), "rm -rf %s_archive", LOG_FILE);
     system(cmd);
 }
-
 void test_file_logging_basic() {
     cleanup();
     assert(init_fileLog(LOG_FILE, 1024, false) == 1);
-
     LOG_INFO("Basic log message");
     close_logging();
-
     FILE* f = fopen(LOG_FILE, "r");
     assert(f != NULL);
-
     char line[512];
     int found = 0;
     while(fgets(line, sizeof(line), f)) {
@@ -44,17 +40,13 @@ void test_file_logging_basic() {
 void test_file_logging_rotation() {
     cleanup();
     assert(init_fileLog(LOG_FILE, 50, false) == 1);
-
     for(int i = 0; i < 20; i++){
         LOG_INFO("Rotating log entry %d", i);
     }
-
     close_logging();
-
     FILE* f = fopen(LOG_FILE, "r");
     assert(f != NULL);
     fclose(f);
-
     char backup[128];
     snprintf(backup, sizeof(backup), "%s.1", LOG_FILE);
     f = fopen(backup, "r");
@@ -66,16 +58,28 @@ void test_file_logging_archive() {
     cleanup();
     assert(init_fileLog(LOG_FILE, 50, true) == 1);
 
-    for(int i = 0; i < 20; i++){
+    for(int i = 0; i < DEFAULT_MAX_BACKUP_FILES * 5; i++) {
         LOG_INFO("Archive log entry %d", i);
     }
 
     close_logging();
 
-    DIR* dir = opendir("test_file_log.txt_archive"); 
+    DIR* dir = opendir("test_file_log.txt_archive");
+    assert(dir != NULL);
     closedir(dir);
-}
 
+    int zip_found = 0;
+    DIR *d = opendir(".");
+    struct dirent *de;
+    while((de = readdir(d))) {
+        if(strstr(de->d_name, "_archive_") && strstr(de->d_name, ".zip")) {
+            zip_found = 1;
+            break;
+        }
+    }
+    closedir(d);
+    assert(zip_found == 1);
+}
 int main() {
     test_file_logging_basic();
     test_file_logging_rotation();
