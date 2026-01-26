120d8d0160cecdeda88270e9b5041235fae4a555
ssszcmawo <belan2470@gmail.com>
2026-01-26 23:26:18 +0100

    Enhance logging with additional log levels
    Added warning, error, and trace log messages to the logger.

--- examples/example_file_logger.c
+++ examples/example_file_logger.c
@@ -1,23 +1,22 @@
 #include "slogger.h"
 
 int main() {
-    // Initializes file-based logging
-    // "logs/log.txt"      -> path to the log file
-    // 1024 * 1024         -> maximum log file size in bytes (1 MB) 
+
     start_logging_thread();
     init_fileLog("logs/log.txt", 1024 * 1024);
 
-    // Sets the minimum log level to DEBUG
-    // All log messages (DEBUG and higher) will be logged
     set_log_level(DEBUG);
 
-    // Logs an informational message to the log file
     LOG_INFO("INFO MESSAGE");
 
-    // Logs a debug message (useful for tracing program execution)
     LOG_DEBUG("DEBUG MESSAGE");
 
-    // Properly closes the logger and flushes the log file
+    LOG_WARN("WARNING MESSAGE");
+
+    LOG_ERROR("ERROR MESSAGE");
+
+    LOG_TRACE("TRACE MESSAGE");
+
     close_logging();
 
     return 0;
